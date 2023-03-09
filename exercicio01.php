e815bebe9fdfbb49309912fd4b120c2af2d28735
Isabelle Cristina <110576073+isabellecristinav@users.noreply.github.com>
2023-03-08 22:03:30 -0300

    exercicios

---
+++ exercicio01.php
@@ -0,0 +1,8 @@
+<?php
+$numero = 0;
+
+while ($numero <= 100){
+    echo $numero . "<br>";
+    $numero = $numero + 2;
+}
+?>
---
+++ exercicio04.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Idade</title>
+</head>
+<body>
+    <form>
+    Informe sua idade: <input type="text" name="num1"><br>
+    <button type="submit">exibir</button>
+    </form>
+
+    <?php
+
+    ?>
+</body>
+</html>
